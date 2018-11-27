ddf83db4e73b5fd0f0bfd796952f2642d54a7e19
unknown <mechleechan@outlook.com>
2018-11-27 23:25:25 +0800

    add search in rotated sorted array solution

---
+++ MyLeetcode/Questions/Remove Duplicates from Sorted List II/RemoveDuplicatesFromSortedListII_solution.cpp
@@ -0,0 +1,26 @@
+
+Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head || !head->next) return head;
+        
+        ListNode *start = new ListNode(0);
+        start->next = head;
+        ListNode *pre = start; // start 用于返回；pre 用于遍历
+		while(pre->next) {
+			ListNode *cur = pre->next;
+			while (cur->next && cur->next->val == cur->val) cur = cur->next; 
+			if (cur != pre->next) pre->next = cur->next;
+			else pre = pre->next;
+		} 
+		return start->next;
+    }
+};
+//while-if-else挺精彩的，逻辑清晰 
